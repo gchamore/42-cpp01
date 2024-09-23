49867b65cfec3b7f57b11da2d848415bc6de70b9
Apos <gchamore@student.42.fr>
2024-09-23 15:02:19 +0200

    ok ex00 cpp01

--- ex00/Zombie.cpp
+++ ex00/Zombie.cpp
@@ -6,13 +6,13 @@
 /*   By: gchamore <gchamore@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/23 11:27:55 by gchamore          #+#    #+#             */
-/*   Updated: 2024/09/23 15:00:08 by gchamore         ###   ########.fr       */
+/*   Updated: 2024/09/23 15:01:56 by gchamore         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Zombie.hpp"
 
-Zombie::Zombie(std::string name): _name(name)
+Zombie::Zombie(std::string name): _name(name) 
 {
 	std::cout << "new zombie " << Zombie::_name << std::endl;
 	return;
